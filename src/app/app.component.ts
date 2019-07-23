b604527af188cd370cd71b185695887deffb52d3
Ignacio Buioli <ibuioli@gmail.com>
2019-07-23 19:49:58 -0300

    feat: recusivity on reading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ declare var window: any;
 export class AppComponent {
   title = 'XML-viewer';
   treeData: any;
+  children: any = [];
+  contCar = 0;
 
   constructor() {}
 
@@ -28,11 +30,27 @@ export class AppComponent {
     }
   }
 
+  public recursive(s: any, c: any): void {
+    console.log('New Structure');
+    for (const item of s.getElementsByTagName('ITEM')) {
+      this.contCar++;
+
+      const nc = {
+        name: item.getAttribute('ID'),
+        parent: s.parentNode.getAttribute('ID'),
+        children: []
+      };
+
+      c.children.push(nc);
+
+      if (item.getElementsByTagName('ESTRUTURA')) {
+        this.recursive(item.getElementsByTagName('ESTRUTURA')[0], nc);
+      }
+    }
+  }
+
   public loadXML(file: any): void {
     const parser: any = new DOMParser();
-    const childs: any = [];
-
-    let contCar = 0;
 
     const xmlDoc = parser.parseFromString(file, 'text/xml');
 
@@ -44,69 +62,71 @@ export class AppComponent {
           const caracs = item.getElementsByTagName('CONFIGURADO')[0].getElementsByTagName('CARACTERISTICA');
           console.log(item.getAttribute('ID'));
 
-	  if (item.getElementsByTagName('ESTRUTURA')) {
-		console.log('ESTRUCTURA');
-	  }
-
           let resposta = '';
           for (const carac of caracs) {
-            if (carac.getAttribute('CODIGO') == 'CODIGOAMBIENTEDEFAULT') {
-		resposta += 'CODIGOAMBIENTEDEFAULT: ' + carac.getAttribute('RESPOSTA') + '\n'
-	    } else if (carac.getAttribute('CODIGO') == 'CODIGOARTICULO') {
-		resposta += 'CODIGOARTICULO: ' + carac.getAttribute('RESPOSTA') + '\n'
-	    } else if (carac.getAttribute('CODIGO') == 'CODIGOCOLOR') { 
-		resposta += 'CODIGOCOLOR: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'CODIGODISTRIBUCION') { 
-		resposta += 'CODIGODISTRIBUCION: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'CODIGOFAMILIA') { 
-		resposta += 'CODIGOFAMILIA: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'CODIGOHERRAJEPRECIO') { 
-		resposta += 'CODIGOHERRAJEPRECIO: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'CODIGOLINEA') { 
-		resposta += 'CODIGOLINEA: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'CODIGOMATERIAL') { 
-		resposta += 'CODIGOMATERIAL: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'CODIGOMODELO') { 
-		resposta += 'CODIGOMODELO: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'CODIGOMODOCONSTRUCTIVO') { 
-		resposta += 'CODIGOMODOCONSTRUCTIVO: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'CODIGOMODOSUSTENTACION') { 
-		resposta += 'CODIGOMODOSUSTENTACION: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'CODIGOPRECIO') { 
-		resposta += 'CODIGOPRECIO: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'CODIGOTIPOENTIDAD') { 
-		resposta += 'CODIGOTIPOENTIDAD: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'CODIGOTIPOMUEBLE') { 
-		resposta += 'CODIGOTIPOMUEBLE: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'CODIGOUBICACIONVERTICALDEFAULT') { 
-		resposta += 'CODIGOUBICACIONVERTICALDEFAULT: ' + carac.getAttribute('RESPOSTA') + '\n'
-            } else if (carac.getAttribute('CODIGO') == 'DESCRIPCIONCOMPLETA') { 
-		resposta += 'DESCRIPCIONCOMPLETA: ' + carac.getAttribute('RESPOSTA') + '\n'
+            if (carac.getAttribute('CODIGO') === 'CODIGOAMBIENTEDEFAULT') {
+            resposta += 'CODIGOAMBIENTEDEFAULT: ' + carac.getAttribute('RESPOSTA') + '\n';
+            } else if (carac.getAttribute('CODIGO') === 'CODIGOARTICULO') {
+            resposta += 'CODIGOARTICULO: ' + carac.getAttribute('RESPOSTA') + '\n';
+            } else if (carac.getAttribute('CODIGO') === 'CODIGOCOLOR') {
+            resposta += 'CODIGOCOLOR: ' + carac.getAttribute('RESPOSTA') + '\n';
+            } else if (carac.getAttribute('CODIGO') === 'CODIGODISTRIBUCION') {
+            resposta += 'CODIGODISTRIBUCION: ' + carac.getAttribute('RESPOSTA') + '\n';
+                    } else if (carac.getAttribute('CODIGO') === 'CODIGOFAMILIA') {
+            resposta += 'CODIGOFAMILIA: ' + carac.getAttribute('RESPOSTA') + '\n';
+                    } else if (carac.getAttribute('CODIGO') === 'CODIGOHERRAJEPRECIO') {
+            resposta += 'CODIGOHERRAJEPRECIO: ' + carac.getAttribute('RESPOSTA') + '\n';
+                    } else if (carac.getAttribute('CODIGO') === 'CODIGOLINEA') {
+            resposta += 'CODIGOLINEA: ' + carac.getAttribute('RESPOSTA') + '\n';
+                    } else if (carac.getAttribute('CODIGO') === 'CODIGOMATERIAL') {
+            resposta += 'CODIGOMATERIAL: ' + carac.getAttribute('RESPOSTA') + '\n';
+                    } else if (carac.getAttribute('CODIGO') === 'CODIGOMODELO') {
+            resposta += 'CODIGOMODELO: ' + carac.getAttribute('RESPOSTA') + '\n';
+                    } else if (carac.getAttribute('CODIGO') === 'CODIGOMODOCONSTRUCTIVO') {
+            resposta += 'CODIGOMODOCONSTRUCTIVO: ' + carac.getAttribute('RESPOSTA') + '\n';
+                    } else if (carac.getAttribute('CODIGO') === 'CODIGOMODOSUSTENTACION') {
+            resposta += 'CODIGOMODOSUSTENTACION: ' + carac.getAttribute('RESPOSTA') + '\n';
+                    } else if (carac.getAttribute('CODIGO') === 'CODIGOPRECIO') {
+            resposta += 'CODIGOPRECIO: ' + carac.getAttribute('RESPOSTA') + '\n';
+                    } else if (carac.getAttribute('CODIGO') === 'CODIGOTIPOENTIDAD') {
+            resposta += 'CODIGOTIPOENTIDAD: ' + carac.getAttribute('RESPOSTA') + '\n';
+                    } else if (carac.getAttribute('CODIGO') === 'CODIGOTIPOMUEBLE') {
+            resposta += 'CODIGOTIPOMUEBLE: ' + carac.getAttribute('RESPOSTA') + '\n';
+                    } else if (carac.getAttribute('CODIGO') === 'CODIGOUBICACIONVERTICALDEFAULT') {
+            resposta += 'CODIGOUBICACIONVERTICALDEFAULT: ' + carac.getAttribute('RESPOSTA') + '\n';
+                    } else if (carac.getAttribute('CODIGO') === 'DESCRIPCIONCOMPLETA') {
+            resposta += 'DESCRIPCIONCOMPLETA: ' + carac.getAttribute('RESPOSTA') + '\n';
             }
           }
-          childs.push({
-            name: item.getAttribute('ID'),
-            parent: 'ROOT',
-            children: [],
-	    resposta: resposta
-          });
+          const childs = {
+              name: item.getAttribute('ID'),
+              parent: 'ROOT',
+              children: [],
+              resposta
+          };
+
+          this.children.push(childs);
+
+          if (item.getElementsByTagName('ESTRUTURA')) {
+            this.recursive(item.getElementsByTagName('ESTRUTURA')[0], childs);
+          }
           console.log('- - - - - - - - - - - - -');
-	  contCar++;
+          this.contCar++;
         }
       }
-
-      console.log('Caract: ' + contCar);
       console.log('- - - - End Read - - - -');
 
-      this.treeData = [
-        {
-          name: 'ROOT',
-          parent: 'null',
-          children: childs
-        }
-      ];
-
-      this.drawGraph(contCar * 34);
+      setTimeout(() => {
+        this.treeData = [
+          {
+            name: 'ROOT',
+            parent: 'null',
+            children: this.children
+          }
+        ];
+        console.log(this.treeData);
+        this.drawGraph(this.contCar * 34);
+      }, 1000);
     } else {
       alert('XML Inválido - Consulte al Administrador');
     }
@@ -148,7 +168,7 @@ export class AppComponent {
       const links = tree.links(nodes);
 
       // Normalize for fixed-depth.
-      nodes.forEach((d: any) => { d.y = d.depth * 100; });
+      nodes.forEach((d: any) => { d.y = d.depth * 200; });
 
       // Update the nodes…
       const node = svg.selectAll('g.node')
